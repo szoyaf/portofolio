23e551f58db6755e55443f683141d36d4b56b4b3
szoyaf <shaneyzoyaf@gmail.com>
2026-04-30 15:37:26 +0700

    chore: server listen to public

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
